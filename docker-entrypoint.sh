35148849a05118d035c1354d0133bc85d3fb68f4
gabornyerges <54465842+gabornyerges@users.noreply.github.com>
2021-01-25 14:14:58 +0100

    add INPUT_PULL_IMAGES_FIRST

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -110,6 +110,10 @@ if ! [ -z "$INPUT_COPY_STACK_FILE" ] && [ $INPUT_COPY_STACK_FILE = 'true' ] ; th
 
   execute_ssh ${DEPLOYMENT_COMMAND} "$INPUT_ARGS" 2>&1
 else
+  if ! [ -z "$INPUT_PULL_IMAGES_FIRST" ] && [ $INPUT_PULL_IMAGES_FIRST = 'true' ] && [ $INPUT_DEPLOYMENT_MODE = 'docker-compose' ] ; then
+    execute_ssh "${DEPLOYMENT_COMMAND} pull"
+  fi
+  
   echo "Connecting to $INPUT_REMOTE_DOCKER_HOST... Command: ${DEPLOYMENT_COMMAND} ${INPUT_ARGS}"
   ${DEPLOYMENT_COMMAND} ${INPUT_ARGS} 2>&1
 fi
