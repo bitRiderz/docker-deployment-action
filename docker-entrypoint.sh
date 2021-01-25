f0332b56943f9f0a3215a5311e3a13c746c9c362
gabornyerges <54465842+gabornyerges@users.noreply.github.com>
2021-01-25 14:46:40 +0100

    rollback

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -110,10 +110,6 @@ if ! [ -z "$INPUT_COPY_STACK_FILE" ] && [ $INPUT_COPY_STACK_FILE = 'true' ] ; th
 
   execute_ssh ${DEPLOYMENT_COMMAND} "$INPUT_ARGS" 2>&1
 else
-  if ! [ -z "$INPUT_PULL_IMAGES_FIRST" ] && [ $INPUT_PULL_IMAGES_FIRST = 'true' ] && [ $INPUT_DEPLOYMENT_MODE = 'docker-compose' ] ; then
-    ${DEPLOYMENT_COMMAND} pull
-  fi
-  
   echo "Connecting to $INPUT_REMOTE_DOCKER_HOST... Command: ${DEPLOYMENT_COMMAND} ${INPUT_ARGS}"
   ${DEPLOYMENT_COMMAND} ${INPUT_ARGS} 2>&1
 fi
